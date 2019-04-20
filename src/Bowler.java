dd8f5f59112836b7b4d800ca3550b3c304562d06
Hersh Nagpal <hxn6993@rit.edu>
2019-04-20 16:00:20 -0400

    Documented and cleaned up Bowler.

--- src/Bowler.java
+++ src/Bowler.java
@@ -21,9 +21,13 @@
  *  Class that holds all bowler info
  *
  */
-
 public class Bowler {
 
+	/**
+	 * fullName: the full name of the bowler
+	 * nickName: the bowler's chosen nickname for scoring
+	 * email: the bowler's email address
+	 */
     private String fullName;
     private String nickName;
     private String email;
@@ -34,25 +38,36 @@ public class Bowler {
   	email = mail;
     }
 
-
+	/**
+	 * Returns this bowler's nickname
+	 * @return this bowler's nickname
+	 */
     public String getNickName() {
-
         return nickName;  
-
     }
 
+	/**
+	 * Returns this bowler's full name
+	 * @return this bowler's full name
+	 */
 	public String getFullName ( ) {
 			return fullName;
 	}
-	
-	public String getNick ( ) {
-		return nickName;
-	}
 
+	/**
+	 * Returns this bowler's email address
+	 * @return this bowler's email address
+	 */
 	public String getEmail ( ) {
 		return email;	
 	}
 	
+
+	/**
+	 * Returns true if this bowler and b are equal, false if they are not.
+	 * @param b the bowler to check equality with
+	 * @return true if this bowler and b are equal, false otherwise
+	 */
 	public boolean equals ( Bowler b) {
 		boolean retval = true;
 		if ( !(nickName.equals(b.getNickName())) ) {
