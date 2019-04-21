bd551f749fd90f927902061b24f3c202189005f3
Hersh Nagpal <hxn6993@rit.edu>
2019-04-20 20:41:48 -0400

    Documented and cleaned up drive class.

--- src/drive.java
+++ src/drive.java
@@ -1,10 +1,17 @@
 import java.util.Vector;
 import java.io.*;
 
+/**
+ * The main runner class of the bowling alley.
+ */
 public class drive {
 
 	public static void main(String[] args) {
 
+		/**
+		 * numLanes: The number of lanes in the alley
+		 * maxPatronsPerParty: the maximum number of patrons that can be in a party
+		 */
 		int numLanes = 3;
 		int maxPatronsPerParty=5;
 
