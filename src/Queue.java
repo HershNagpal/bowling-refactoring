137cca42b580adcefb33060ec5f2f777e57dc7df
Hersh Nagpal <hxn6993@rit.edu>
2019-04-21 17:23:25 -0400

    Documented the Queue class.

--- src/Queue.java
+++ src/Queue.java
@@ -10,29 +10,49 @@
  
 import java.util.Vector;
  
+/**
+ * Wrapper for the Vector class to make them easier to deal with in a sequential manner.
+ */
 public class Queue {
+	/**
+	 * v: the vector being wrapped as a Queue
+	 */
 	private Vector v;
 	
-	/** Queue()
-	 * 
-	 * creates a new queue
+	/** 
+	 * Creates a new queue around a vector object
 	 */
 	public Queue() {
 		v = new Vector();
 	}
-	
+
+	/**
+	 * Retrieves the next object sequentially in the Queue.
+	 * @return the object directly after the current object
+	 */
 	public Object next() {
 		return v.remove(0);
 	}
 
+	/**
+	 * Adds a new object to the Queue
+	 */
 	public void add(Object o) {
 		v.addElement(o);
 	}
 	
+	/**
+	 * Checks whether the Queue has any more elements left in it.
+	 * @return true if the Queue has elements, false if it is empty.
+	 */
 	public boolean hasMoreElements() {
 		return v.size() != 0;
 	}
 
+	/**
+	 * Returns the Queue as a Vector.
+	 * @return the unwrapped Vector beneath the Queue implementation.
+	 */
 	public Vector asVector() {
 		return v;
 	}
